48c31a8e44575d63a5946dde87c12efc6cc4ba1c
Adam Lewis <balast@users.noreply.github.com>
2020-07-06 22:30:39 -0500

    add bits about downloading pretrained model

--- docs/usage.rst
+++ docs/usage.rst
@@ -2,12 +2,18 @@
 Usage
 =====
 
-SaliencyDetector instances are iterator, so you can use them accordingly. E.g.
+A pretrained resnet50 model and Poolnet saliency detector model are needed for this package to work properly.  They can be downloaded from the following links.
 
-.. code-block:: python
+* Resnet50 Model: `<https://drive.google.com/open?id=1Q2Fg2KZV8AzNdWNjNgcavffKJBChdBgy>`_
+* Poolnet Model: `<https://drive.google.com/file/d/1sH5RKEt6SnG33Z4sI-hfLs2d21GmegwR/view>`_
+
+Extract the .pth files afterwards, and pass in the locations when instantiating the SaliencyDetector object.
+
+.. code-block:: python3
 
     from saliency_detector import SaliencyDetector
-    sal_det = SaliencyDetector()
+    sal_det = SaliencyDetector(pretrained_resnet50_path='mypath_to/resnet50_caffe.pth',
+                               pretrained_saldet_model_path='mypath_to/final.pth')
     result1 = sal_det.solver.predict(img[0])
     result2 = sal_det.solver.predict(img[1])
 
--- saliency_detector/__init__.py
+++ saliency_detector/__init__.py
@@ -5,11 +5,11 @@ import torch
 from saliency_detector.dataset.joint_dataset import get_loader
 from saliency_detector.joint_solver import Solver
 
-__version__ = "0.2.3"
+__version__ = "0.2.5"
 
 config_attr_defaults = {'cuda': None,
                         'mode': 'test',
-                        'model': 'saliency_detector/results/run-1/models/final.pth',
+                        'model': None,
                         'test_mode': 1,
                         'num_thread': 0,
                         'image_paths': None,
@@ -18,29 +18,28 @@ config_attr_defaults = {'cuda': None,
                         'batch_size': 1,
                         'arch': 'resnet',
                         'load': '',
-                        'pretrained_model': 'saliency_detector/dataset/pretrained/resnet50_caffe.pth'}
+                        'pretrained_model': None}
 
 Config = namedtuple('Config', config_attr_defaults.keys(), defaults=config_attr_defaults.values())
 
 
 class SaliencyDetector():
-    def __init__(self, cuda=None):
+    def __init__(self, pretrained_resnet50_path, pretrained_saldet_model_path, cuda=None):
         """
 
         Parameters
         ----------
-        image_paths: list of paths to images
-        cuda
-        kwargs
+        pretrained_resnet50_path: Path or string
+        pretrained_saldet_model_path: Path or string
+        cuda: bool
         """
         if cuda is None:
             # autodetect if gpu is available, if not specified
             cuda = True if torch.cuda.is_available() else False
 
-        # image_paths = [Path(image_path) if not isinstance(image_path, Path) else image_path
-        #                            for image_path in image_paths]
-        # test_list = [image_path.name for image_path in image_paths]
-        self.config = Config(cuda=cuda, mode='test', num_thread=0, image_paths=None, test_list=None)
+
+        self.config = Config(cuda=cuda, mode='test', num_thread=0, image_paths=None, test_list=None,
+                             model=str(pretrained_saldet_model_path), pretrained_model=str(pretrained_resnet50_path))
         # test_loader = get_loader(self.config, mode='test')
         # self.config._replace(test_loader=test_loader)
 
